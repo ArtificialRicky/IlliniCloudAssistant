788171cff4908fa4da2ed64b379937a610904f27
Eric Liu <eric1932777@gmail.com>
2022-11-16 19:53:47 -0600

    [feat] User create/read

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import motor.motor_asyncio
 from fastapi import FastAPI
+from fastapi import status
+from starlette.responses import JSONResponse
+
+MONGODB_URL = "mongodb+srv://root:IlliniCloudAssistant@cluster0.7oprx89.mongodb.net/test"
 
 app = FastAPI()
+
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
+db = client.IlliniCloudAssistant
 
 
 @app.get("/")
@@ -14,18 +22,48 @@ async def say_hello(name: str):
 
 
 # 注册账号
-@app.post("/register")
+@app.api_route("/register", methods=["GET", "POST"])
 async def register(username: str, password: str):
-    # TODO 写入{user} {pass}到数据库
-    return {"message": f"register {username} {password}"}
+    """
+    写入 username, password 到数据库
+    :param username:
+    :param password:
+    :return:
+    """
+
+    user = {
+        "username": username,
+        "password": password,
+    }
+    new_user = await db["User"].insert_one(user)
+    created_user = await db["User"].find_one({
+        "_id": new_user.inserted_id,
+    })
+    # return {"message": f"register {username} {password}"}
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_user)
 
 
 # 登录
 @app.get("/login")
 async def login(username: str, password: str):
-    token = ""
     # TODO 从数据库中读取{user} {pass}，如果匹配成功，生成token
-    return token
+    token = "I'm token"
+
+    if not username or not password:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
+                            content={"message": "username or password is empty"})
+
+    user = await db["User"].find_one({
+        "username": username,
+        "password": password,
+    })
+
+    if user is not None:
+        return JSONResponse(status_code=status.HTTP_200_OK, content={"token": token})
+    else:
+        # user is None
+        return JSONResponse(status_code=status.HTTP_403_FORBIDDEN,
+                            content={"message": "Invalid username or password"})
 
 
 # 修改密码
